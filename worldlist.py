76bf5529a0a8c0596b4a1081801fd6080fde462e
MrTeukuAzhar <48705454+MrTeukuAzhar@users.noreply.github.com>
2019-03-19 11:26:55 +0700

    Add files via upload

---
+++ worldlist.py
@@ -0,0 +1,134 @@
+import time
+azhar = True
+while azhar:
+    pasha = input('Nama Pengguna>')
+    if pasha == "azhar":
+        azhar = False
+    elif pasha == "faisal":
+        azhar = False
+    else:
+        azhar = True
+        
+gntl = True
+while gntl:
+    gt = input('Passwordnya>')
+    if gt == "tuanazhar":
+        gntl = False
+    else:
+        gntl = True
+print("____________________________________________________________________")
+print("##          ##    ##        ## ##    ## ######## ########### ########")
+print("##          ##    ##        ## ##    ##    ##        ##      ##")
+print("##          ##    ##        ## ##    ##    ##        ##      ##")
+print("##    ##    ##    ##   ##   ## ########    ##        ##      ##c#####")
+print(" ##  ## ##  ## ### ## ## ## ## ##    ##    ##        ##      ##")
+print("  ###     ##   ###  ##    ##   ##    ## ########     ##      ########")
+print("---------------------------------------------------------------------")
+print('''
+Makers:Azhar_Pasha
+Contact Me:azhargntl1@gmail.com
+Since:2018
+CopyRight 2019
+Note:Membuat Ini Karena diri Gua Garing:
+''')
+nama = input('Siapa nama lengkap Target>')
+nad = input('Nama Depan>')
+nab = input('Nama Belakang>')
+tgl = input('Tanggal Lahir')
+np = input('Nama pet>')
+npt = input('tanggal pet>')
+pc = input('Nama Pacar>')
+dk = input('Nama Adik>')
+sh = input('Nama Sahabat>')
+pg = input('Nama Panggilan>')
+gratis = True
+while gratis:
+    gra = input("Tulis Kode Program Ini>")
+    if gra == "221205":
+        gratis = False
+    else:
+        gratis = True
+print('Memulai Proses Unpacking 0%')
+time.sleep(3)
+print('Memulai Proses Unpacking 6%')
+time.sleep(3)
+print('Memulai Proses Unpacking 15%')
+time.sleep(3)
+print('Memulai Proses Unpacking 25%')
+time.sleep(3)
+print('Memulai Proses Unpacking 50%')
+time.sleep(3)
+print('Memulai Proses Unpacking 65%')
+time.sleep(3)
+print('Memulai Proses Unpacking 70%')
+time.sleep(3)
+print('Memulai Proses Unpacking 75%')
+time.sleep(3)
+print('Memulai Proses Unpacking 80%')
+time.sleep(3)
+print('Memulai Proses Unpacking 100%')
+time.sleep(2)
+print('############################')
+print(nama)
+time.sleep(2)
+print(nad)
+time.sleep(2)
+print(nab)
+time.sleep(2)
+print(tgl)
+time.sleep(2)
+print(np)
+time.sleep(2)
+print(npt)
+time.sleep(2)
+print(pc)
+time.sleep(2)
+print(dk)
+time.sleep(2)
+print(sh)
+time.sleep(2)
+print(pg)
+time.sleep(2)
+print(nad+nab)
+time.sleep(2)
+print(nad+tgl)
+time.sleep(2)
+print(pg+tgl)
+time.sleep(2)
+print(nad+tgl)
+time.sleep(2)
+print(np+npt)
+time.sleep(2)
+print(pg+pc)
+time.sleep(2)
+print(pg+dk)
+time.sleep(2)
+print(pg+sh)
+time.sleep(2)
+print(nama+tgl)
+time.sleep(2)
+print(nad+pc)
+time.sleep(2)
+print(nab+pc)
+time.sleep(2)
+print('Tambahan')
+print(nab,123)
+time.sleep(2)
+print(nab,124)
+time.sleep(2)
+print(nab,125)
+time.sleep(2)
+print(nab,pc)
+time.sleep(2)
+print(nad,125)
+time.sleep(2)
+print(nad,126)
+time.sleep(2)
+print(nad,127)
+time.sleep(2)
+print(nad,128)
+time.sleep(2)
+print(nad,129)
+time.sleep(2)
+print(nad,130)
+time.sleep(2)
